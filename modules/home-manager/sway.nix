bac45d81698e1660c3afc73f92ad672cd5c16a27
trickypr <git@trickypr.com>
2024-10-03 19:01:44 +1000

    Fix cursors

--- modules/home-manager/sway.nix
+++ modules/home-manager/sway.nix
@@ -21,6 +21,13 @@ in
     programs.kitty.enable = true;
     catppuccin.pointerCursor.enable = false;
 
+    home.pointerCursor =  {
+      gtk.enable = true;
+      package = pkgs.posy-cursors;
+      name = "Posy_Cursor_Black";
+      size = 22;
+    };
+
     wayland.windowManager.sway = {
       enable = true;
       catppuccin.enable = true;
