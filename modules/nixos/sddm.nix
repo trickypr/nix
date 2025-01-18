0294e87d878209199dfb78cf3751e4cc7dad9265
trickypr <git@trickypr.com>
2025-01-18 12:04:35 +1100

    ask nicely for sway to be the default dm

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -26,8 +26,9 @@ in
     services.gnome.gnome-keyring.enable = cfg.keyring;
     security.pam.services.sddm.enableGnomeKeyring = cfg.keyring;
 
-    # Sway appear in the list
+    # Sway appear in the list and it the default session
     programs.sway.enable = cfg.sway;
+    services.displayManager.defaultSession = "Sway";
 
     # kanshi systemd service
     systemd.user.services.kanshi = lib.mkIf cfg.sway {
